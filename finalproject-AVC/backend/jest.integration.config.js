cf9dacf004f9611f78828461f3ff4f0a554042cc
Jorge Pilo <jorgeluispilotorres@gmail.com>
2026-03-10 10:00:35 -0600

    Revert "Merge pull request #111 from valeriianaa/feature/entrga3-AVC"
    This reverts commit 5d918c52aafc1cca41e1bb3183d3a2b911d1231f, reversing
    changes made to 57cc05774111abbbff1545aa63b981f8c9d83744.

--- finalproject-AVC/backend/jest.integration.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/tests/integration'],
-    testMatch: ['**/*.test.ts'],
-    setupFilesAfterEnv: ['<rootDir>/tests/integration/setup.ts'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
